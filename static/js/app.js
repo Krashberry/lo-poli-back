a9db4c32c723c7353e7593c9b81ecfd06f9931c7
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2020-09-20 21:50:07 -0700

    Moving things around a bit.

--- static/js/app.js
+++ static/js/app.js
@@ -1,47 +1,73 @@
 // add refs for the image tags 
+let tColorPic = document.getElementById("t_color_1")
+let tColor = document.getElementById("t_color")
+
+let tBorderPic = document.getElementById("t_border_1")
+let tBorder = document.getElementById("t_border")
+
 let sknPic = document.getElementById("s_color_1")
 let sColor = document.getElementById("s_color")
+
 let eyesPic = document.getElementById("eyes_1")
 let eyes = document.getElementById("eyes")
+
+let eColorPic = document.getElementById("e_color_1")
 let eColor = document.getElementById("e_color")
+
+let nosePic = document.getElementById("nose_1")
 let nose = document.getElementById("nose")
+
+let mouthPic = document.getElementById("mouth_1")
 let mouth = document.getElementById("mouth")
+
+let hairPic = document.getElementById("hair_1")
 let hair = document.getElementById("hair")
+
+let hColorPic = document.getElementById("h_color_1")
 let hColor = document.getElementById("h_color")
-let fHair = document.getElementById("f_hair")
+
+let fhColorPic = document.getElementById("fhc_1")
 let fhColor = document.getElementById("fhc")
+
+let fHairPic = document.getElementById("f_hair_1")
+let fHair = document.getElementById("f_hair")
+
+let itemPic = document.getElementById("item_1")
 let item = document.getElementById("item")
-let tColor = document.getElementById("t_color")
-let tBorder = document.getElementById("t_border")
 
 sColor.addEventListener("change", e => {
   let sColorElement = document.querySelector("#s_color").value
-  sknPic.setAttribute("src", `https://lo-poli.s3-us-west-2.amazonaws.com/${sColorElement}.jpg`)
+  sknPic.setAttribute("src", `https://lo-poli.s3-us-west-2.amazonaws.com/${sColorElement}.png`)
 });
 
 eyes.addEventListener("change", e => {
   let eyesElement = document.querySelector("#eyes").value
-  eyesPic.setAttribute("src", `https://lo-poli.s3-us-west-2.amazonaws.com/${eyesElement}.jpg`)
+  eyesPic.setAttribute("src", `https://lo-poli.s3-us-west-2.amazonaws.com/${eyesElement}.png`)
 });
 
 eColor.addEventListener("change", e => {
-  showPic.innerHTML = e.target.value  
+  let eColorElement = document.querySelector("#e_color").value
+  eColorPic.setAttribute("src", `https://lo-poli.s3-us-west-2.amazonaws.com/${eColorElement}.png`)  
 });
 
 nose.addEventListener("change", e => {
-  showPic.innerHTML = e.target.value  
+  let noseElement = document.querySelector("#nose").value
+  nosePic.setAttribute("src", `https://lo-poli.s3-us-west-2.amazonaws.com/${noseElement}.png`)  
 });
 
 mouth.addEventListener("change", e => {
-  showPic.innerHTML = e.target.value  
+  let mouthElement = document.querySelector("#mouth").value
+  mouthPic.setAttribute("src", `https://lo-poli.s3-us-west-2.amazonaws.com/${mouthElement}.png`) 
 });
 
 hair.addEventListener("change", e => {
-  showPic.innerHTML = e.target.value  
+  let hairElement = document.querySelector("#hair").value
+  hairPic.setAttribute("src", `https://lo-poli.s3-us-west-2.amazonaws.com/${hairElement}.jpg`)  
 });
 
 hColor.addEventListener("change", e => {
-  showPic.innerHTML = e.target.value  
+  let hColorElement = document.querySelector("#eyes").value
+  hColorPic.setAttribute("src", `https://lo-poli.s3-us-west-2.amazonaws.com/${hColorElement}.jpg`)  
 });
 
 fHair.addEventListener("change", e => {
